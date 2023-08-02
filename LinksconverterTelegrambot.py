068f6ffcadb900c82e59693692c22a09a371ec2c
Shashwat  Kumar Das <121427522+ShashwatKumar01@users.noreply.github.com>
2023-08-02 17:29:41 +0530

    Update LinksconverterTelegrambot.py

--- LinksconverterTelegrambot.py
+++ LinksconverterTelegrambot.py
@@ -171,7 +171,7 @@ def send_message_to_all(message_text, media):
 
 @bot.message_handler(commands=['showlist'])
 def show_user_lists(message):
-    usercount = len(user_data)
+    usercount = str(len(user_data))
     userlists = str(user_data)
     bot.reply_to(message, f"users lists are: {userlists} ")
     bot reply_to(message, f" number of users :{usercount}")
